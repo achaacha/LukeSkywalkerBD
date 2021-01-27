0d42ae3c517413108f2a58bd00715ea506056099
Acha <43567591+achaacha@users.noreply.github.com>
2021-01-26 22:24:38 -0600

    Add files via upload

--- SaviorSkywalker.plugin.js
+++ SaviorSkywalker.plugin.js
@@ -1,1 +1,16 @@
-d
+/**
+ * @name SaviorSkywalker
+ * @author Acha
+ * @authorId 164744231879966720
+ * @version 0.1.0
+ * @invite TUdjaNKa
+ * @website https://github.com/achaacha/SaviorSkywalker
+ * @source https://raw.githubusercontent.com/achaacha/SaviorSkywalker/main/SaviorSkywalker.plugin.js
+ * @updateUrl https://raw.githubusercontent.com/achaacha/SaviorSkywalker/main/SaviorSkywalker.plugin.js
+ */
+
+
+ module.exports = (_ => {
+   start() {};
+   stop() {};
+ })();
